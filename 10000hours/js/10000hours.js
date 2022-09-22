2b71b95f1a01ad7ba2e329f5034e160d3247abb2
wjdtngus1237 <wjdtngus1237@naver.com>
2022-09-22 18:17:41 +0900

    10000hours 수정

--- 10000hours/js/10000hours.js
+++ 10000hours/js/10000hours.js
@@ -7,12 +7,16 @@ const getResultSelect = document.querySelector('.result-select');
 const getResultHours = document.querySelector('.hours-result-select');
 
 function whoAmI(event) {
-    event.preventDefault();
-    removeClass.classList.remove('hidden');
-    const myToDo = getExpertForm.value;
-    const myTime = getHoursPrac.value;
-    getResultSelect.innerText = myToDo;
-    getResultHours.innerText = 10000 / myTime;
+    if (getHoursPrac.value == NaN) {
+        alert('숫자를 입력해주세요');
+    } else {
+        event.preventDefault();
+        removeClass.classList.remove('hidden');
+        const myToDo = getExpertForm.value;
+        const myTime = getHoursPrac.value;
+        getResultSelect.innerText = myToDo;
+        getResultHours.innerText = 10000 / myTime;
+    }
 }
 
 getWannabeForm.addEventListener('submit', whoAmI);
