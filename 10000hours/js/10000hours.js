eeccd5141ffc930cd491a82857e1294fddb25b0d
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2022-10-15 17:16:31 +0900

    10000시간 시계돌아가는거 구현

--- 10000hours/js/10000hours.js
+++ 10000hours/js/10000hours.js
@@ -5,18 +5,22 @@ const getHoursPrac = document.querySelector('.hours-prac');
 const removeClass = document.querySelector('.result-container');
 const getResultSelect = document.querySelector('.result-select');
 const getResultHours = document.querySelector('.hours-result-select');
+const getImgContainer = document.querySelector('.loading-img-container');
+
+const HIDDEN = 'hidden';
 
 function whoAmI(event) {
-    if (getHoursPrac.value == NaN) {
-        alert('숫자를 입력해주세요');
-    } else {
-        event.preventDefault();
-        removeClass.classList.remove('hidden');
+    event.preventDefault();
+    getImgContainer.classList.remove(HIDDEN);
+
+    setTimeout(function () {
+        removeClass.classList.remove(HIDDEN);
+        getImgContainer.classList.add(HIDDEN);
         const myToDo = getExpertForm.value;
         const myTime = getHoursPrac.value;
         getResultSelect.innerText = myToDo;
-        getResultHours.innerText = 10000 / myTime;
-    }
+        getResultHours.innerText = parseInt(10000 / myTime);
+    }, 2000);
 }
 
 getWannabeForm.addEventListener('submit', whoAmI);
